4c633233a0af1cca901ac377a1c74b4fa68e5294
Win Rawin <rawin@belusys.com>
2026-07-10 22:32:08 +0700

    update

---
+++ start.bat
@@ -0,0 +1,2 @@
+docker-compose -f ./docker-compose.yml up -d
+docker ps | grep -E 'ecard-web|ecard-web'
---
+++ stop.bat
@@ -0,0 +1,2 @@
+docker-compose -f ./docker-compose.yml down
+docker ps | grep -E 'ecard-web|ecard-web'
